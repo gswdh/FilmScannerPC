d717a7546b0caa494d1c8ec517320cec8471a38a
George Waller <georgewaller@gswdh.co.uk>
2017-04-02 18:54:49 +0100

    Inital project, playing with GUI.

---
+++ mainwindow.cpp
@@ -0,0 +1,45 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+
+    delete ui;
+}
+
+void MainWindow::on_pushButtonStart_clicked()
+{
+    QString scan_resolution = ui->lineEditReso->text();
+    QString scan_frames = ui->lineEditFrames->text();
+
+
+
+    int resolution = scan_resolution.toInt();
+    int frames = scan_frames.toInt();
+
+    int seconds = int(resolution) * int(resolution) * 1.5 * int(frames) * 1.1 / 1000000;
+
+    QString out_seconds = QString::number(seconds % 60);
+    QString out_minutes = QString::number(seconds / 60);
+
+    QString msg;
+    msg += "Scan beginning at ";
+    msg += scan_resolution;
+    msg += "dpi for ";
+    msg += scan_frames;
+    msg += " frames. Scan time is ";
+    msg += out_minutes;
+    msg += "m ";
+    msg += out_seconds;
+    msg += "s.";
+
+    ui->labelOutput->setText(msg);
+}
