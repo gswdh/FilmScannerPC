741b46f04507fd27dd4418ec5124b08cc8f6ce01
George Waller <georgewaller@gswdh.co.uk>
2017-10-01 17:34:56 +0100

    Working FTDI class.

---
+++ FilmScanner/ftdi.cpp
@@ -0,0 +1,86 @@
+#include "ftdi.h"
+
+ftdi::ftdi()
+{
+
+
+
+}
+
+int ftdi::ListDevices(FTDIDevices *devices)
+{
+
+    return 0;
+}
+
+int ftdi::Connect(char serial[])
+{
+    // Open device by serial number
+    ftStatus = FT_OpenEx(serial, FT_OPEN_BY_SERIAL_NUMBER, &ftHandle);
+
+    // Reset the FTDI device
+    ftStatus = FT_SetBitMode(ftHandle, Mask, 0);
+
+    // Delay for the device to restart
+    Sleep(100);
+
+    // Set the mode to 245 sync
+    ftStatus = FT_SetBitMode(ftHandle, Mask, MODE_SYNC_245);
+
+    // Configure latency
+    ftStatus = FT_SetLatencyTimer(ftHandle, LatencyTimer);
+
+    // Configure USB params
+    ftStatus = FT_SetUSBParameters(ftHandle, 32768, 0);
+
+    // Set the flow control
+    ftStatus = FT_SetFlowControl(ftHandle, FT_FLOW_RTS_CTS, 0, 0);
+
+    // Status message
+    if(ftStatus == FT_OK) return 0;
+    else return 1;
+}
+
+int ftdi::Disconnect()
+{
+    // Close the device
+    ftStatus = FT_Close(ftHandle);
+
+    // Status message
+    if(ftStatus == FT_OK) return 0;
+    else return 1;
+}
+
+int ftdi::Write(FTDIData data)
+{
+    // Check to see if we're connected to a device
+    if(ftHandle == NULL) return 3;
+
+    // Number of bytes sent
+    DWORD nbytes_tx = 0;
+
+    // Write the FTDI device
+    ftStatus = FT_Write(ftHandle, data.buffer, data.buff_len, &nbytes_tx);
+
+    // Status message
+    if(nbytes_tx != data.buff_len) return 2;
+
+    // Status message
+    if(ftStatus == FT_OK) return 0;
+    else return 1;
+}
+
+
+
+int ftdi::Read(FTDIData *data)
+{
+    // Check to see if we're connected to a device
+    if(ftHandle == NULL) return 2;
+
+    // Attempt to read the maximum amount of data
+    ftStatus = FT_Read(ftHandle, data->buffer, 65535, &data->buff_len);
+
+    // Status message
+    if(ftStatus == FT_OK) return 0;
+    else return 1;
+}
---
+++ FilmScanner/ftdi.h
@@ -0,0 +1,44 @@
+#ifndef FTDI_H
+#define FTDI_H
+
+#include "ftd2xx.h"
+
+// Data variable
+struct FTDIData{
+    char buffer[65535];
+    int buff_len = 0;
+};
+
+// Devices variable
+struct FTDIDevices{
+    char device1[32];
+    char device2[32];
+    char device3[32];
+    char device4[32];
+    int ndevs = 0;
+};
+
+class ftdi
+{
+private:
+    // Internal variables
+    DWORD LatencyTimer = 100;
+    DWORD deviceID;
+
+    UCHAR MODE_SYNC_245 = 0x40;
+    UCHAR Mask = 0xFF;
+
+    FT_HANDLE ftHandle;
+    FT_STATUS ftStatus;
+    FT_DEVICE ftDevice;
+
+public:
+    ftdi();
+    int ListDevices(FTDIDevices *devices);
+    int Connect(char serial[]);
+    int Disconnect();
+    int Write(FTDIData data);
+    int Read(FTDIData *data);
+};
+
+#endif // FTDI_H
